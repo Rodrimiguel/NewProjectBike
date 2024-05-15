bff3b6963959dc33a35005ebba07826495648212
RoderickMFoggia <rodrigomfoggia84@gmail.com>
2024-05-15 06:40:36 -0300

    Update App.Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AutosService } from './services/autos.service';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,11 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'ProjectBike';
+
+constructor(private autosService : AutosService){
+  console.log(autosService.getAutos());
 }
+
+
+}
+
